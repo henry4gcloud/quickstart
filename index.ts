c61f89210c374a06db9607892ab5bb4b471ec59a
gotasource <gotasource@gmail.com>
2019-01-09 09:53:57 +0700

    update config

--- index.ts
+++ index.ts
@@ -7,15 +7,7 @@ import {GotaApp, GotaBoot} from "@gota/boot";
     config: {
         hostName : 'localhost',
         port: 3001,
-        devMode:true,
-        database: {
-            protocol:'mongodb+srv',
-            host: 'cluster0-g6wi8.gcp.mongodb.net/test?retryWrites=true',
-            user: 'admin',
-            password: 'iC2gjdMkgrjDwF03',
-            // options:{},
-            databaseName:'gota'
-        },
+        devMode:true
     }
 })
 class StoreApp{};
